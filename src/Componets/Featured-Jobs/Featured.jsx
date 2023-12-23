e4300b11b91c5563759111404a5e98753551d07c
Md Minhaj <parsonal322532@gmail.com>
2023-12-24 02:35:55 +0600

    6 job Added

--- src/Componets/Featured-Jobs/Featured.jsx
+++ src/Componets/Featured-Jobs/Featured.jsx
@@ -9,14 +9,18 @@ const Featured = () => {
         .then(res => res.json())
         .then(data => setJobs(data))
     }, []);
+
     return (
-        <div className="container mx-auto px-3 md:px-0">
-            <h1>Featured Jobs</h1>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            <div>
+        <div className="container mx-auto px-3 md:px-0 pb-20">
+            <h1 className="text-center text-zinc-800 text-3xl md:text-5xl font-semibold font-['Manrope'] mb-4">Featured Jobs</h1>
+            <p className="text-center text-neutral-500 text-base font-medium font-['Manrope'] leading-relaxed">Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className="grid md:grid-cols-2 gap-[24px] py-10">
                 {
                     jobs.map(job => <FeacturedJobItem key={job.id} job={job}></FeacturedJobItem>)
                 }
+            </div>
+            <div className="text-center">
+                <button className="px-6 py-4 bg-gradient-to-r from-indigo-400 to-violet-500 rounded-lg text-white text-xl font-extrabold font-['Manrope']">See All Jobs</button>
             </div>
         </div>
     );
--- src/Componets/Fectured-Job/FeacturedJobItem.jsx
+++ src/Componets/Fectured-Job/FeacturedJobItem.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
+import Location from '../../assets/icons/Location.png';
 
 const FeacturedJobItem = ({job}) => {
     const {logo, job_title, company_name, remote_or_onsite, job_type, location, salary} = job;
     return (
-        <div>
+        <div className="rounded-lg border border-gray-200 p-[40px]">
             <img src={logo} alt="" />
+            <h2 className="text-zinc-700 text-2xl font-extrabold font-['Manrope'] mt-[25px] mb-[8px]">{job_title}</h2>
+            <h4 className="text-neutral-500 text-xl font-semibold font-['Manrope']">{company_name}</h4>
+            <div className="flex gap-4 py-[16px]">
+                <button className="text-indigo-400 text-base font-extrabold font-['Manrope'] px-[19px] py-[9px] rounded border border-indigo-400 hover:bg-indigo-500 hover:text-white">{remote_or_onsite}</button>
+                <button className="text-indigo-400 text-base font-extrabold font-['Manrope'] px-[19px] py-[9px] rounded border border-indigo-400 hover:bg-indigo-500 hover:text-white">{job_type}</button>
+            </div>
+            <div className="flex gap-6 text-neutral-500 text-xl font-semibold font-['Manrope']">
+                <p className='flex gap-2'>
+                    <img src={Location} alt="" />
+                    {location}
+                </p>
+                <p>{salary}</p>
+            </div>
+            <div className="mt-[24px]">
+                <Link to="/">
+                    <button className="text-white text-xl font-extrabold font-['Manrope'] px-[18px] py-[11px] bg-gradient-to-r from-indigo-400 to-violet-500 rounded">View Details</button>
+                </Link>
+            </div>
         </div>
     );
 };
